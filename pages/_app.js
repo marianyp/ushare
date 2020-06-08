db6f7e373f64af5f1da87dc53558585136244241
Mariany Pinkston <marianypinkston@gmail.com>
2020-06-08 18:22:36 -0400

    viewport meta changes

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,13 @@ export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<title>uShare — Share Posts Easily Over Text & Social Media</title>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				<title>
+					uShare — Share Posts Easily Over Text & Social Media
+				</title>
 			</Head>
 			<Component {...pageProps} />
 		</>
