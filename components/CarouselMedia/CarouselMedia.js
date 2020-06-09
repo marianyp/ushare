36e9063937b03a4720ff3b958ed944848fc9c985
Mariany Pinkston <marianypinkston@gmail.com>
2020-06-09 14:24:13 -0400

    added useefect to carousel media

--- components/CarouselMedia/CarouselMedia.js
+++ components/CarouselMedia/CarouselMedia.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react"
+import { useRef, useState, useEffect } from "react"
 import Media from '../Media/Media'
 
 export default function CarouselMedia({ shareData }) {
